8a603a6717c8f77ccc54ae46a7cbdeb62bb839c9
Asiaviscardi <asia.viscardii@gmail.com>
2023-09-21 15:05:13 +0200

    inserimento object nelle card

---
+++ script.js
@@ -0,0 +1,35 @@
+const persona1 = {name:"Wayne Barnett", role:"Founder & CEO"}
+
+document.getElementById("wayne").innerHTML = persona1.name
+
+document.getElementById("founder").innerHTML = persona1.role
+
+const persona2 = {name:"Angela Caroll", role:"Chief Editor"}
+
+document.getElementById("angelac").innerHTML = persona2.name
+
+document.getElementById("chief").innerHTML = persona2.role
+
+const persona3 = {name:"Walter Gordon", role:"Office Manager"}
+
+document.getElementById("walter").innerHTML = persona3.name
+
+document.getElementById("office").innerHTML = persona3.role
+
+const persona4 = {name:"Angela Lopez", role:"Social Media Manager"}
+
+document.getElementById("angelal").innerHTML = persona4.name
+
+document.getElementById("social").innerHTML = persona4.role
+
+const persona5 = {name:"Scott Estrada", role:"Developer"}
+
+document.getElementById("scott").innerHTML = persona5.name
+
+document.getElementById("developer").innerHTML = persona5.role
+
+const persona6 = {name:"Barbara Ramos", role:"Graphic Designer"}
+
+document.getElementById("barbara").innerHTML = persona6.name
+
+document.getElementById("graphic").innerHTML = persona6.role
