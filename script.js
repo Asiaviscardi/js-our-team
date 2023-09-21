77a71738951b19bd260ed57e0fb18aae6f3855c2
Asiaviscardi <asia.viscardii@gmail.com>
2023-09-21 21:48:05 +0200

    termine esercizio con modifiche

--- script.js
+++ script.js
@@ -32,61 +32,42 @@ const team = [
     
 ];
 
-const h3 = document.querySelector("h3")
+const row = document.querySelector(".row");
 
-
-for (let key of team) { 
-    const names = document.getElementsByName("h3")
-
-    console.log(h3)
-
-    const image = document.createElement("img");
-        image.setAttribute("src", "img/" + team[key]);
-        h3.append(image);
+for(let i in team){
+  const Member = team[i];
+  const col = document.createElement("div");
+  col.classList.add("col");
+  row.append(col);
+  for(let key in Member){
+    console.log(key)
+    switch (key) {
+      case "name":
+        const name = document.createElement("h2");
+        name.classList.add("py-2");
+        name.innerHTML =`
+        ${Member[key]}
+        `
+        col.append(name);
+        break;
+        
+      case "role":
+        const role = document.createElement("p");
+        role.classList.add("pb-4");
+        role.innerHTML =`
+        ${Member[key]}
+        `
+        col.append(role);
+        break;
+          
+      case "image":
+        const image = document.createElement("img");
+        image.setAttribute("src", "img/" + Member[key]);
+        col.append(image);
+        break;
+        
+        default:
+          break;
+    }
   }
-  
-
-
-
-
-
-
-
-
-
-
-
-
-//document.getElementById("wayne").innerHTML = persona1.name
-
-//document.getElementById("founder").innerHTML = persona1.role
-
-//const persona2 = {name:"Angela Caroll", role:"Chief Editor"}
-
-//document.getElementById("angelac").innerHTML = persona2.name
-
-//document.getElementById("chief").innerHTML = persona2.role
-
-//const persona3 = {name:"Walter Gordon", role:"Office Manager"}
-
-//document.getElementById("walter").innerHTML = persona3.name
-
-//document.getElementById("office").innerHTML = persona3.role
-
-//const persona4 = {name:"Angela Lopez", role:"Social Media Manager"}
-
-//document.getElementById("angelal").innerHTML = persona4.name
-
-//document.getElementById("social").innerHTML = persona4.role
-
-//const persona5 = {name:"Scott Estrada", role:"Developer"}
-
-//document.getElementById("scott").innerHTML = persona5.name
-
-//document.getElementById("developer").innerHTML = persona5.role
-
-//const persona6 = {name:"Barbara Ramos", role:"Graphic Designer"}
-
-//document.getElementById("barbara").innerHTML = persona6.name
-
-//document.getElementById("graphic").innerHTML = persona6.role
+}
